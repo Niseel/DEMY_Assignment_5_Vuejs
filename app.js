3f4afe9721e98616e81167e6f34f002da705e1f4
Thanh NguyenCong <nguyencongthanh200199@gmail.com>
2023-07-23 14:20:18 +0700

    Submit Assignment 6

--- app.js
+++ app.js
@@ -1,30 +1,105 @@
+function getRandomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const app = Vue.createApp({
   data() {
     return {
-      enterTask: "",
-      tasks: [],
-      isShow: true,
+      maximunMonsterHP: 100,
+      maximunplayerHP: 100,
+      monsterHP: 100,
+      playerHP: 100,
+      round: 0,
+      winner: null,
+      logMessages: [],
     };
   },
-  computed: {
-    renderCaption() {
-      return this.isShow ? "Show" : "Hide";
+  watch: {
+    monsterHP(value) {
+      if (value <= 0 && this.playerHP <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Monster lose
+        this.winner = "player";
+      }
+      return value;
     },
-    renderTaskList() {
-      return this.isShow;
+    playerHP(value) {
+      if (value <= 0 && this.monsterHP <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Player lose
+        this.winner = "monster";
+      }
+      return value;
+    },
+  },
+  computed: {
+    renderMonsterHP() {
+      const nextHealValue = this.monsterHP >= 0 ? this.monsterHP : 0;
+      return { width: nextHealValue + "%" };
+    },
+    renderPlayerHP() {
+      const nextHealValue = this.playerHP >= 0 ? this.playerHP : 0;
+      return { width: nextHealValue + "%" };
+    },
+    unlockSpecialAttack() {
+      return this.round && this.round % 3 === 0 ? false : true;
     },
   },
   methods: {
-    addTask() {
-      if (this.enterTask) {
-        this.tasks.push(this.enterTask);
-        this.enterTask = "";
-      }
+    playerAttack() {
+      this.round++;
+      const attackValue = getRandomNumberBetween(2, 10);
+      this.monsterHP = this.monsterHP - attackValue;
+      this.addLogData("Player", "Attack", attackValue);
+      this.monsterAttack();
     },
-    reRender() {
-      this.isShow = !this.isShow;
+    playerSpecialAttack() {
+      this.round++;
+      const attackValue = getRandomNumberBetween(20, 50);
+      this.monsterHP = this.monsterHP - attackValue;
+      this.addLogData("Player", "Special Attack", attackValue);
+    },
+    playerHeal() {
+      this.round++;
+      const healValue = getRandomNumberBetween(10, 15);
+      this.playerHP =
+        this.playerHP + healValue >= this.smaximunPlayerHP
+          ? this.smaximunPlayerHP
+          : this.playerHP + healValue;
+      this.addLogData("Player", "heal", healValue);
+      this.monsterAttack();
+    },
+    playerSurrender() {
+      this.playerHP = 0;
+      this.addLogData("Player", "Surrender", 0);
+    },
+    monsterAttack() {
+      const attackValue = getRandomNumberBetween(5, 10);
+      this.playerHP = this.playerHP - attackValue;
+      this.addLogData("Monster", "Attack", attackValue);
+    },
+
+    resetData() {
+      this.monsterHP = 100;
+      this.playerHP = 100;
+      this.round = 0;
+      this.winner = null;
+      this.addLogData("Game", "Reset", 0);
+      this.addLogData("--------", "----------", "------");
+    },
+
+    addLogData(who, action, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: action,
+        actionValue: value,
+      });
     },
   },
 });
 
-app.mount("#assignment");
+app.mount("#game");
